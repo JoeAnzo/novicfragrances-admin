d8843d0c3d4d6fd7adf4b56b1a21d4711eb2748f
Joel Anzovule <joelanzovule@gmail.com>
2026-09-13 04:59:19 -0400

    edited logo

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { loginSchema, type LoginInput } from "../../../schemas/login.schema";
 import { EyeCloseIcon, EyeIcon } from "../../icons";
+import Logo from "./images/logo/logo.jpeg";
 import Label from "../form/Label";
 import Input from "../form/input/InputField";
 import Checkbox from "../form/input/Checkbox";
@@ -34,13 +35,21 @@ export default function SignInForm() {
       <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
         <div>
           <div className="mb-5 sm:mb-8">
-            <span className="text-lg text-center font-semibold tracking-[0.2em] text-gray-900 dark:text-white">
-              NOVIC
-            </span>
-            <br/>
-            <span className="mb-2 text-center text-[9px]  uppercase tracking-[0.35em] text-gray-500 dark:text-white">
-              fragrances
-            </span>
+            <div className="flex items-center justify-center gap-2">
+              <div>
+                <img src={Logo} alt="NOVIC Logo" className="w-16 h-16 rounded-md" />
+              </div>
+              <div>
+                <span className="text-lg text-center font-semibold tracking-[0.2em] text-gray-900 dark:text-white">
+                  NOVIC
+                </span>
+                <br/>
+                <span className="mb-2 text-center text-[9px]  uppercase tracking-[0.35em] text-gray-500 dark:text-white">
+                  fragrances
+                </span>
+              </div>
+            </div>
+            
             <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
               Sign In
             </h1>
